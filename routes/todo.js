3ab3229780d5583fe618bfbc354777d983b9d7da
PremDev324 <rpremkumar324@gmail.com>
2025-07-29 10:46:34 +0530

    Todo App

---
+++ routes/todo.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/Todo");
+const todo = require("../controllers/todo");
+
+router.get("/", todo.homeController);
+
+router.get("/add-todo", todo.addTodoFormController);
+
+router.get("/update-todo", todo.updateTodoFormController);
+
+router.get("/delete-todo", todo.deleteTodoPageController);
+
+router.post("/add-todo", todo.addTodoController);
+
+router.post("/update-todo/:id", todo.updateTodoController);
+
+router.get("/confirm-delete", todo.deleteTodoController);
+
+module.exports = router;
